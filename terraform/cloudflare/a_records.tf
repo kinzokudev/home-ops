a7baff6ae88bdbd94641ea00b374a295f37518fc
kinzokudev <kinzoku@the-nebula.xyz>
2025-04-01 20:06:50 -0400

    test new terraform workflows

--- terraform/cloudflare/a_records.tf
+++ terraform/cloudflare/a_records.tf
@@ -6,3 +6,12 @@ resource "cloudflare_dns_record" "main" {
   content = module.onepassword_cloudflare.fields["VERCEL_IPV4_ADDRESS"]
   zone_id = module.onepassword_cloudflare.fields["CLOUDFLARE_ZONE_ID"]
 }
+
+resource "cloudflare_dns_record" "www_main" {
+  name = "www"
+  proxied = true
+  ttl = 1
+  type = "A"
+  content = module.onepassword_cloudflare.fields["VERCEL_IPV4_ADDRESS"]
+  zone_id = module.onepassword_cloudflare.fields["CLOUDFLARE_ZONE_ID"]
+}
