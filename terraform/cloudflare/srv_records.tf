a71909f257998161a77e22636b3fe43be417a275
kinzokudev <kinzoku@the-nebula.xyz>
2025-04-06 11:53:47 -0400

    chore(tf/cloudflare/srv): move priority into data field

--- terraform/cloudflare/srv_records.tf
+++ terraform/cloudflare/srv_records.tf
@@ -2,8 +2,8 @@ resource "cloudflare_dns_record" "email_imap_incoming" {
   name = "_imaps._tcp.kinzoku.dev"
   ttl = 1
   type = "SRV"
-  priority = 0
   data = {
+    priority = 0
     weight = 1
     port = 993
     target = "imap.migadu.com"
@@ -15,8 +15,8 @@ resource "cloudflare_dns_record" "email_pop3_incoming" {
   name = "_pop3s._tcp.kinzoku.dev"
   ttl = 1
   type = "SRV"
-  priority = 0
   data = {
+    priority = 0
     weight = 1
     port = 995
     target = "pop.migadu.com"
@@ -28,8 +28,8 @@ resource "cloudflare_dns_record" "email_smtp_outgoing" {
   name = "_submissions._tcp.kinzoku.dev"
   ttl = 1
   type = "SRV"
-  priority = 0
   data = {
+    priority = 0
     weight = 1
     port = 465
     target = "smtp.migadu.com"
@@ -41,8 +41,8 @@ resource "cloudflare_dns_record" "email_outlook_autodiscover" {
   name = "_autodiscover._tcp.kinzoku.dev"
   ttl = 1
   type = "SRV"
-  priority = 0
   data = {
+    priority = 0
     weight = 1
     port = 443
     target = "autodiscover.migadu.com"
