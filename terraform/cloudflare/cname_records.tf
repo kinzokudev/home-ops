05149d1d728a50d8b222d1d0f429004bb48ba5d0
kinzokudev <kin@kinzoku.dev>
2025-04-26 19:55:32 -0400

    tf|cloudflare: change mc cname record
    Signed-off-by: kinzokudev <kin@kinzoku.dev>

--- terraform/cloudflare/cname_records.tf
+++ terraform/cloudflare/cname_records.tf
@@ -1,71 +1,71 @@
 resource "cloudflare_dns_record" "main" {
-  name = "@"
+  name    = "@"
   proxied = true
-  ttl = 1
-  type = "CNAME"
+  ttl     = 1
+  type    = "CNAME"
   content = "kinzokudev.github.io"
   zone_id = module.onepassword_cloudflare.fields["CLOUDFLARE_ZONE_ID"]
 }
 
 resource "cloudflare_dns_record" "www_main" {
-  name = "www"
+  name    = "www"
   proxied = true
-  ttl = 1
-  type = "CNAME"
+  ttl     = 1
+  type    = "CNAME"
   content = "kinzokudev.github.io"
   zone_id = module.onepassword_cloudflare.fields["CLOUDFLARE_ZONE_ID"]
 }
 
 resource "cloudflare_dns_record" "blog" {
-  name = "blog"
+  name    = "blog"
   proxied = true
-  ttl = 1
-  type = "CNAME"
+  ttl     = 1
+  type    = "CNAME"
   content = "kinzokudev.github.io"
   zone_id = module.onepassword_cloudflare.fields["CLOUDFLARE_ZONE_ID"]
 }
 
 resource "cloudflare_dns_record" "mc" {
-  name = "mc"
+  name    = "mc"
   proxied = false
-  ttl = 1
-  type = "CNAME"
-  content = "variety-enhancement.gl.at.ply.gg"
+  ttl     = 1
+  type    = "CNAME"
+  content = "27.ip.gl.ply.gg"
   zone_id = module.onepassword_cloudflare.fields["CLOUDFLARE_ZONE_ID"]
 }
 
 resource "cloudflare_dns_record" "email_primary_key" {
-  name = "key1._domainkey"
+  name    = "key1._domainkey"
   proxied = false
-  ttl = 1
-  type = "CNAME"
+  ttl     = 1
+  type    = "CNAME"
   content = "key1.kinzoku.dev._domainkey.migadu.com"
   zone_id = module.onepassword_cloudflare.fields["CLOUDFLARE_ZONE_ID"]
 }
 
 resource "cloudflare_dns_record" "email_secondary_key" {
-  name = "key2._domainkey"
+  name    = "key2._domainkey"
   proxied = false
-  ttl = 1
-  type = "CNAME"
+  ttl     = 1
+  type    = "CNAME"
   content = "key2.kinzoku.dev._domainkey.migadu.com"
   zone_id = module.onepassword_cloudflare.fields["CLOUDFLARE_ZONE_ID"]
 }
 
 resource "cloudflare_dns_record" "email_tertiary_key" {
-  name = "key3._domainkey"
+  name    = "key3._domainkey"
   proxied = false
-  ttl = 1
-  type = "CNAME"
+  ttl     = 1
+  type    = "CNAME"
   content = "key3.kinzoku.dev._domainkey.migadu.com"
   zone_id = module.onepassword_cloudflare.fields["CLOUDFLARE_ZONE_ID"]
 }
 
 resource "cloudflare_dns_record" "email_autoconfig" {
-  name = "autoconfig"
+  name    = "autoconfig"
   proxied = false
-  ttl = 1
-  type = "CNAME"
+  ttl     = 1
+  type    = "CNAME"
   content = "autoconfig.migadu.com"
   zone_id = module.onepassword_cloudflare.fields["CLOUDFLARE_ZONE_ID"]
 }
